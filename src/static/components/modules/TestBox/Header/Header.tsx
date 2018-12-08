a805765c668bf3ba5799a8b1d17144c4aa16bf08
Olga <olga.myazina@outlook.com>
2018-12-08 15:18:26 +0300

    chore: добавлена кнопка Run

--- src/static/components/modules/TestBox/Header/Header.tsx
+++ src/static/components/modules/TestBox/Header/Header.tsx
@@ -1,11 +1,21 @@
 import * as React from 'react';
 
-import Octicon, { ChevronDown, ChevronUp, Clippy } from '@githubprimer/octicons-react';
+import Octicon, {
+  ChevronDown,
+  ChevronUp,
+  Clippy,
+} from '@githubprimer/octicons-react';
 import Text from 'src/components/ui/Text/Text';
-import { ClipboardStyled, ControlsStyled, ButtonIconContainerStyled, HeaderContainerStyled } from './styled';
+import {
+  ClipboardStyled,
+  ControlsStyled,
+  ButtonIconContainerStyled,
+  HeaderContainerStyled,
+} from './styled';
 
 import { HeaderProps, HeaderState } from './types';
 import { ColorType } from 'src/components/ui/types';
+import Button from 'src/components/ui/Button/Button';
 
 class Header extends React.PureComponent<HeaderProps, HeaderState> {
   private getTitle = () => this.props.title;
@@ -16,7 +26,11 @@ class Header extends React.PureComponent<HeaderProps, HeaderState> {
     return status === 'fail' || status === 'error' ? 'red' : 'green';
   }
 
-  private getChevron = () => this.props.isOpenedBox ? ChevronUp : ChevronDown;
+  private getChevron = () => (this.props.isOpenedBox ? ChevronUp : ChevronDown);
+
+  public handleRunClick() {
+    console.log('run test-retries');
+  }
 
   public render(): JSX.Element {
     const { title } = this.props;
@@ -25,15 +39,28 @@ class Header extends React.PureComponent<HeaderProps, HeaderState> {
     const Chevron = this.getChevron();
 
     return (
-      <HeaderContainerStyled className={'Box-header d-flex flex-justify-between flex-items-center p-3'}>
+      <HeaderContainerStyled
+        className={
+          'Box-header d-flex flex-justify-between flex-items-center p-3'
+        }
+      >
         <Text as={'span'} textColor={textColor} textType={'bold'}>
           {title}
         </Text>
         <ControlsStyled>
+          <Button
+            size={'sm'}
+            className={'mr-3'}
+            title={'Run'}
+            onClick={this.handleRunClick}
+          />
           <ClipboardStyled component='div' option-text={this.getTitle}>
             <Octicon icon={Clippy} />
           </ClipboardStyled>
-          <ButtonIconContainerStyled role={'button'} onClick={this.props.onToggle}>
+          <ButtonIconContainerStyled
+            role={'button'}
+            onClick={this.props.onToggle}
+          >
             <Octicon icon={Chevron} />
           </ButtonIconContainerStyled>
         </ControlsStyled>
