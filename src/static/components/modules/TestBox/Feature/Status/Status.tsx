842dbd94fb36bedfd82bd13eea4728bd454d312c
Olga <olga.myazina@outlook.com>
2018-12-08 16:07:37 +0300

    fix: корректный статус строки с названием браузера

--- src/static/components/modules/TestBox/Feature/Status/Status.tsx
+++ src/static/components/modules/TestBox/Feature/Status/Status.tsx
@@ -8,22 +8,51 @@ import { BrowserNameStyled } from './styled';
 
 import { StatusProps } from './types';
 import { ColorType } from 'src/components/ui/types';
+import { fail } from 'assert';
 
 export default class Status extends React.PureComponent<StatusProps> {
-  public cnStatus = cn(this.props.className, 'd-flex flex-justify-between flex-items-center');
+  public cnStatus = cn(
+    this.props.className,
+    'd-flex flex-justify-between flex-items-center',
+  );
   public status = this.props.status;
 
-  public isFail = status === 'fail' || status === 'error';
-  public statusColor: ColorType = this.isFail ? 'red' : 'green';
+  public isFail = () => {
+    if (this.status === 'fail') {
+      return true;
+    }
+    if (this.status === 'error') {
+      return true;
+    }
+    return false;
+  }
+
+  public statusColor: ColorType = this.isFail() ? 'red' : 'green';
   public maxPage = this.props.data.attempt + 1;
 
   public render() {
-    const { onClickAtTitle, pageCount, handleDataChange, pageCurrent, title } = this.props;
-
+    const {
+      onClickAtTitle,
+      pageCount,
+      handleDataChange,
+      pageCurrent,
+      title,
+    } = this.props;
     return (
-      <Text as='span' className={this.cnStatus} textColor={this.statusColor} textType='bold'>
-        <StatusIcon mr={2} isFail={this.isFail} />
-        <BrowserNameStyled as='span' textType='bold' textColor={this.statusColor} mr={6} onClick={onClickAtTitle}>
+      <Text
+        as='span'
+        className={this.cnStatus}
+        textColor={this.statusColor}
+        textType='bold'
+      >
+        <StatusIcon mr={2} isFail={this.isFail()} />
+        <BrowserNameStyled
+          as='span'
+          textType='bold'
+          textColor={this.statusColor}
+          mr={6}
+          onClick={onClickAtTitle}
+        >
           {title}
         </BrowserNameStyled>
         <Text as={'span'} textColor={'gray'} mr={2}>
