6562e16b61db3e6e4f0cebbb341496f892f25bf0
mtsymlov <mtsymlov@yandex.ru>
2018-12-08 17:55:22 +0300

    build: build app setup (#17)

--- src/static/webpack.prod.js
+++ src/static/webpack.prod.js
@@ -2,23 +2,25 @@
 
 const webpack = require('webpack');
 const merge = require('webpack-merge');
+const path = require('path');
 
 const commonConfig = require('./webpack.common');
 
-module.exports = merge(
-    commonConfig,
-    {
-        plugins: [
-            new webpack.optimize.UglifyJsPlugin({
-                uglifyOptions: {
-                    compress: {
-                        warnings: false,
-                        'drop_console': true,
-                        unsafe: true
-                    }
-                }
-            }),
-            new webpack.EnvironmentPlugin(['NODE_ENV'])
-        ]
-    }
-);
+module.exports = merge(commonConfig, {
+  output: {
+    filename: 'report.min.js',
+    path: path.resolve(__dirname, '..', '..', 'lib/static')
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      uglifyOptions: {
+        compress: {
+          warnings: false,
+          drop_console: true,
+          unsafe: true
+        }
+      }
+    }),
+    new webpack.EnvironmentPlugin(['NODE_ENV'])
+  ]
+});
