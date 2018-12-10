bea714a240ba0a4364c7215b36b32e2407f78261
mtsymlov <mtsymlov@yandex.ru>
2018-12-10 13:42:11 +0300

    build: поднял вебпак до 4 версии + добавил npm script для общей сборки

--- src/static/webpack.common.js
+++ src/static/webpack.common.js
@@ -1,17 +1,12 @@
 'use strict';
 
 const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlWebpackIncludeAssetsPlugin = require('html-webpack-include-assets-plugin');
 
 const staticPath = path.resolve(__dirname);
 
 module.exports = {
-    entry: {
-      report: ['./index.tsx'],
-      gui: ['./index.tsx']
-    },
     context: staticPath,
     output: {
         filename: '[name].min.js',
@@ -33,25 +28,18 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                loader: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: [{
-                        loader: 'css-loader',
-                        options: {minimize: true}
-                    }]
-                })
+                use: [
+                    "style-loader",
+                    MiniCssExtractPlugin.loader,
+                    "css-loader"
+                ]
             }
         ]
     },
     plugins: [
-        new ExtractTextPlugin('[name].min.css'),
+        new MiniCssExtractPlugin('[name].min.css'),
         new HtmlWebpackPlugin({
             template: 'template.html'
-        }),
-        new HtmlWebpackIncludeAssetsPlugin({
-            files: ['index.html'],
-            assets: ['data.js'],
-            append: false
         })
     ]
 };
--- src/static/webpack.dev.js
+++ src/static/webpack.dev.js
@@ -2,20 +2,30 @@
 
 const webpack = require('webpack');
 const merge = require('webpack-merge');
+const HtmlWebpackIncludeAssetsPlugin = require('html-webpack-include-assets-plugin');
 
 const commonConfig = require('./webpack.common');
 
 module.exports = merge(
     commonConfig,
     {
+        mode: 'development',
+        entry: [
+          './index.tsx'
+        ],
         devtool: 'eval-source-map',
         devServer: {
             contentBase: './',
             inline: true,
-            hot: true
+            hot: true,
         },
         plugins: [
-            new webpack.HotModuleReplacementPlugin()
-        ]
+            new webpack.HotModuleReplacementPlugin(),
+            new HtmlWebpackIncludeAssetsPlugin({
+                files: ['index.html'],
+                assets: ['data.js'],
+                append: false
+            })
+          ]
     }
 );
