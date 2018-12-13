d2296d85a7da18275edc83cb53efe93784be639e
Olga <olga.myazina@outlook.com>
2018-12-13 12:33:33 +0300

    fix: баг после мержа

--- src/static/store/modules/app/actions.ts
+++ src/static/store/modules/app/actions.ts
@@ -17,3 +17,30 @@ export const setTestsViewMode = (value: TestsViewMode) => ({
   type: actionNames.SET_TESTS_VIEW_MODE,
   payload: value,
 });
+
+export const setIsOpenForTestBox = (isOpen: boolean, uuid: string) => ({
+  type: actionNames.SET_IS_OPEN_FOR_TEST_BOX,
+  payload: { isOpen, uuid },
+});
+export const setIsOpenForBrowser = (
+  isOpenedBrowser: boolean,
+  browsersId: string,
+) => ({
+  type: actionNames.SET_IS_OPEN_FOR_BROWSER,
+  payload: { isOpenedBrowser, browsersId },
+});
+export const setIsOpenForView = (
+  isOpenScreenView: boolean,
+  screenViewId: string,
+) => ({
+  type: actionNames.SET_IS_OPEN_FOR_VIEW,
+  payload: { isOpenScreenView, screenViewId },
+});
+
+export const setScreenModForView = (
+  screenViewMod: string,
+  screenViewModId: string,
+) => ({
+  type: actionNames.SET_SCREEN_FOR_VIEW,
+  payload: { screenViewMod, screenViewModId },
+});
