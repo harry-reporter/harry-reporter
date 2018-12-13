8b2570a2ba04317bec3da31beb385c655e0ba56b
Mikhail Durmanov <madurmanov@gmail.com>
2018-12-13 16:07:35 +0300

    feat: add accept button

--- src/static/components/modules/TestBox/Browser/Controls/types.ts
+++ src/static/components/modules/TestBox/Browser/Controls/types.ts
@@ -1,10 +1,12 @@
 import { Attempt } from 'src/store/modules/tests/types';
 
 export interface ControlsProps {
+  isGui: boolean;
   isOpenedBrowser: boolean;
   data: Attempt;
   handleViewChange: (e: string) => void;
   onToggle: () => any;
+  onAccept: () => any;
   viewType: string;
   url: string;
 }
--- src/static/components/modules/TestBox/Browser/types.ts
+++ src/static/components/modules/TestBox/Browser/types.ts
@@ -1,11 +1,13 @@
 import { Browser, Attempt, TypeView } from 'src/store/modules/tests/types';
 
 export interface BrowserProps {
+  isGui: boolean;
   data: Browser;
   url?: string;
   isOpenedBrowser?: boolean;
   setIsOpenForBrowser?: (isOpen: boolean, uuid: string) => void;
   measure?: () => any;
+  onAccept: (browserId, attempt, stateName?) => void;
 }
 export interface BrowserState {
   viewType: TypeView;
